2c203b4f8096bf40333c07f04d12c5a3aeba55f4
Leon <xiaoyankeji@hotmail.com>
2018-10-30 23:36:09 +0800

    增加日志文件实体、监视规则解析结果实体；

---
+++ xQuantLogFactory/Model/LogFile.cs
@@ -0,0 +1,14 @@
+﻿namespace xQuantLogFactory.Model
+{
+    /// <summary>
+    /// 日志文件
+    /// </summary>
+    public class LogFile
+    {
+        /// <summary>
+        /// 日志文件名称
+        /// </summary>
+        public string FileName { get; set; }
+
+    }
+}
---
+++ xQuantLogFactory/Model/MonitorItemResult.cs
@@ -0,0 +1,58 @@
+﻿namespace xQuantLogFactory.Model
+{
+    /// <summary>
+    /// 监视规则解析结果
+    /// </summary>
+    public class MonitorItemResult
+    {
+        /// <summary>
+        /// 日志文件名称
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// 日志写入时间
+        /// </summary>
+        public string LogTime { get; set; }
+
+        /// <summary>
+        /// 监控项目名称
+        /// </summary>
+        public string ItemName { get; set; }
+
+        /// <summary>
+        /// 匹配模式
+        /// </summary>
+        public string Pattern { get; set; }
+
+        /// <summary>
+        /// 日志文件中的行号
+        /// </summary>
+        public int LineNumber { get; set; }
+
+        /// <summary>
+        /// 程序版本
+        /// </summary>
+        public string Version { get; set; }
+
+        /// <summary>
+        /// IP地址
+        /// </summary>
+        public string IPAddress { get; set; }
+
+        /// <summary>
+        /// 日志级别
+        /// </summary>
+        public string Level { get; set; }
+
+        /// <summary>
+        /// 日志内容
+        /// </summary>
+        public string LogContent { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2} {3}", this.FileName, this.Pattern, this.LogTime, this.LogContent);
+        }
+    }
+}
