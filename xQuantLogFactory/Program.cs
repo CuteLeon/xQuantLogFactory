347a50f27c6da0c8297696c19bfb3f93795cdbb6
Leon <xiaoyankeji@hotmail.com>
2018-10-30 13:44:16 +0800

    引用了Form的Console程序；

--- xQuantLogFactory/Program.cs
+++ xQuantLogFactory/Program.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace xQuantLogFactory
 {
     class Program
     {
+        /// <summary>
+        /// 入口
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.Title = $"xQuant 日志分析工具";
-
+            Console.Title = $"xQuant 日志分析工具 - {Application.ProductVersion}";
+            
             Console.Read();
         }
     }
