c179495edef4013ff132caea3e83a617152f18f4
Leon <xiaoyankeji@hotmail.com>
2018-11-20 14:52:48 +0800

    实现一个使用堆栈扫描树的通用方法，外部传入action调用；用上述方法改写初始化子节点表名的方法；

--- xQuantLogFactory/Model/Monitor/MonitorContainer.cs
+++ xQuantLogFactory/Model/Monitor/MonitorContainer.cs
@@ -26,36 +26,59 @@ namespace xQuantLogFactory.Model.Monitor
         public int ContainerID { get; set; }
 
         /// <summary>
-        /// 初始化
+        /// 初始化子节点表名
         /// </summary>
         public void InitMonitorSheetName()
         {
+            this.ScanMonitor((rootStack, currentMonitor) =>
+                {
+                    //遇到表名不为空的表名，初始化其子节点表名
+                    if (!string.IsNullOrEmpty(currentMonitor.SheetName))
+                    {
+                        //应用表名并入栈
+                        currentMonitor.MonitorTreeRoots.ForEach(monitor =>
+                        {
+                            monitor.SheetName = currentMonitor.SheetName;
+                            if (monitor.HasChildren) rootStack.Push(monitor);
+                        });
+                    }
+                    else
+                    {
+                        //仅入栈
+                        currentMonitor.MonitorTreeRoots.ForEach(monitor =>
+                        {
+                            if (monitor.HasChildren) rootStack.Push(monitor);
+                        });
+                    }
+                });
+        }
+
+        /// <summary>
+        /// 通用树深度优先扫描方法
+        /// </summary>
+        /// <param name="scanAction">扫描Action</param>
+        /// <param name="stackInitPredicate">首批根节点入栈条件</param>
+        /// <remarks>Action 参数分别为父级节点堆栈和当前节点，每次执行时顶级节点会出栈，下次需要扫描的子节点入栈即可</remarks>
+        /// <example>使用方法见上方初始化子节点表名的方法</example>
+        public void ScanMonitor(Action<Stack<MonitorItem>, MonitorItem> scanAction, Predicate<MonitorItem> stackInitPredicate = null)
+        {
+            if (scanAction == null)
+                throw new ArgumentNullException(nameof(scanAction));
+
             Stack<MonitorItem> monitorRoots = new Stack<MonitorItem>();
             MonitorItem currentMonitor = null;
-            this.MonitorTreeRoots.ForEach(root => monitorRoots.Push(root));
+
+            //初始化栈
+            ((stackInitPredicate == null) ?
+                this.MonitorTreeRoots :
+                this.MonitorTreeRoots.FindAll(stackInitPredicate))
+                .ForEach(root => monitorRoots.Push(root));
 
             while (monitorRoots.Count > 0)
             {
                 currentMonitor = monitorRoots.Pop();
 
-                //遇到表名不为空的表名，初始化其子节点表名
-                if (!string.IsNullOrEmpty(currentMonitor.SheetName))
-                {
-                    //应用表名并入栈
-                    currentMonitor.MonitorTreeRoots.ForEach(monitor =>
-                    {
-                        monitor.SheetName = currentMonitor.SheetName;
-                        if (monitor.HasChildren) monitorRoots.Push(monitor);
-                    });
-                }
-                else
-                {
-                    //仅入栈
-                    currentMonitor.MonitorTreeRoots.ForEach(monitor =>
-                    {
-                        if (monitor.HasChildren) monitorRoots.Push(monitor);
-                    });
-                }
+                scanAction(monitorRoots, currentMonitor);
             }
         }
 
