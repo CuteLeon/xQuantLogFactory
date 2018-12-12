94dc629756281ab25cb01651568191aadb48c37d
Leon <xiaoyankeji@hotmail.com>
2018-12-12 19:07:29 +0800

    using;

--- xQuantLogFactory/BIZ/Analysiser/GroupAnalysiser/ReportAsyncGroupAnalysiser.cs
+++ xQuantLogFactory/BIZ/Analysiser/GroupAnalysiser/ReportAsyncGroupAnalysiser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+
 using xQuantLogFactory.Model;
 using xQuantLogFactory.Model.Fixed;
 using xQuantLogFactory.Model.Monitor;
