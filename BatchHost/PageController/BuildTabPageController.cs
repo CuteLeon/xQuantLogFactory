49cf9044bd2dced03d0cfc1f88f8f25b61941d5b
Leon <xiaoyankeji@hotmail.com>
2018-12-18 18:26:51 +0800

    update gui;

--- BatchHost/PageController/BuildTabPageController.cs
+++ BatchHost/PageController/BuildTabPageController.cs
@@ -7,10 +7,5 @@ namespace BatchHost
 {
     public partial class BatchHostForm
     {
-        private void BuildControlPanel_Resize(object sender, EventArgs e)
-        {
-            this.BuildDirTextBox.Width = this.BuildButton.Left - this.BuildDirTextBox.Left - 20;
-            this.BuildDirTextBox.TextBoxWidth = this.BuildDirTextBox.Width - 60;
-        }
     }
 }
