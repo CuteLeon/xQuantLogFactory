e05c2ea00563abbbd721e30a93795bda0e168a6d
Leon <xiaoyankeji@hotmail.com>
2018-12-18 13:36:37 +0800

    增加批量宿主项目；

---
+++ BatchHost/BatchHostForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BatchHost
+{
+    public partial class BatchHostForm : Form
+    {
+        public BatchHostForm()
+        {
+            this.InitializeComponent();
+
+            this.Icon = UnityResource.BatchHostIcon;
+        }
+    }
+}
