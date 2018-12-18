e4936d16e969d67d838fe8071412016fbbd9d86e
Leon <xiaoyankeji@hotmail.com>
2018-12-18 14:32:28 +0800

    换用 VisualPlus，雪藏分支；

--- BatchHost/BatchHostForm.Designer.cs
+++ BatchHost/BatchHostForm.Designer.cs
@@ -28,7 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.visualGauge1 = new VisualPlus.Toolkit.Controls.DataVisualization.VisualGauge();
+            this.visualToggle1 = new VisualPlus.Toolkit.Controls.Interactivity.VisualToggle();
             this.SuspendLayout();
+            // 
+            // visualGauge1
+            // 
+            this.visualGauge1.BackColorState.Disabled = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.visualGauge1.BackColorState.Enabled = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(244)))), ((int)(((byte)(249)))));
+            this.visualGauge1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.visualGauge1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.visualGauge1.LargeChange = 5;
+            this.visualGauge1.Location = new System.Drawing.Point(547, 311);
+            this.visualGauge1.Margin = new System.Windows.Forms.Padding(6);
+            this.visualGauge1.Maximum = 100;
+            this.visualGauge1.MaximumVisible = true;
+            this.visualGauge1.Minimum = 0;
+            this.visualGauge1.MinimumVisible = true;
+            this.visualGauge1.MouseState = VisualPlus.Enumerators.MouseStates.Normal;
+            this.visualGauge1.Name = "visualGauge1";
+            this.visualGauge1.Progress = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(136)))), ((int)(((byte)(45)))));
+            this.visualGauge1.ProgressVisible = true;
+            this.visualGauge1.Size = new System.Drawing.Size(174, 117);
+            this.visualGauge1.SmallChange = 1;
+            this.visualGauge1.TabIndex = 0;
+            this.visualGauge1.Text = "visualGauge1";
+            this.visualGauge1.TextStyle.Disabled = System.Drawing.Color.FromArgb(((int)(((byte)(131)))), ((int)(((byte)(129)))), ((int)(((byte)(129)))));
+            this.visualGauge1.TextStyle.Enabled = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.visualGauge1.TextStyle.Hover = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.visualGauge1.TextStyle.Pressed = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.visualGauge1.TextStyle.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.visualGauge1.TextStyle.TextLineAlignment = System.Drawing.StringAlignment.Center;
+            this.visualGauge1.TextStyle.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            this.visualGauge1.Thickness = 25;
+            // 
+            // visualToggle1
+            // 
+            this.visualToggle1.BackColorState.Disabled = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.visualToggle1.BackColorState.Enabled = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(244)))), ((int)(((byte)(249)))));
+            this.visualToggle1.Border.Color = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.visualToggle1.Border.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(183)))), ((int)(((byte)(230)))));
+            this.visualToggle1.Border.HoverVisible = true;
+            this.visualToggle1.Border.Rounding = 20;
+            this.visualToggle1.Border.Thickness = 1;
+            this.visualToggle1.Border.Type = VisualPlus.Enumerators.ShapeTypes.Rounded;
+            this.visualToggle1.Border.Visible = true;
+            this.visualToggle1.ButtonBorder.Color = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.visualToggle1.ButtonBorder.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(183)))), ((int)(((byte)(230)))));
+            this.visualToggle1.ButtonBorder.HoverVisible = true;
+            this.visualToggle1.ButtonBorder.Rounding = 18;
+            this.visualToggle1.ButtonBorder.Thickness = 1;
+            this.visualToggle1.ButtonBorder.Type = VisualPlus.Enumerators.ShapeTypes.Rounded;
+            this.visualToggle1.ButtonBorder.Visible = true;
+            this.visualToggle1.ButtonColorState.Disabled = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.visualToggle1.ButtonColorState.Enabled = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.visualToggle1.ButtonColorState.Hover = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.visualToggle1.ButtonColorState.Pressed = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.visualToggle1.ButtonSize = new System.Drawing.Size(20, 20);
+            this.visualToggle1.FalseTextToggle = "No";
+            this.visualToggle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.visualToggle1.Location = new System.Drawing.Point(688, 277);
+            this.visualToggle1.MouseState = VisualPlus.Enumerators.MouseStates.Normal;
+            this.visualToggle1.Name = "visualToggle1";
+            this.visualToggle1.ProgressImage = null;
+            this.visualToggle1.Size = new System.Drawing.Size(50, 25);
+            this.visualToggle1.TabIndex = 1;
+            this.visualToggle1.Text = "visualToggle1";
+            this.visualToggle1.TextStyle.Disabled = System.Drawing.Color.FromArgb(((int)(((byte)(131)))), ((int)(((byte)(129)))), ((int)(((byte)(129)))));
+            this.visualToggle1.TextStyle.Enabled = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.visualToggle1.TextStyle.Hover = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.visualToggle1.TextStyle.Pressed = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.visualToggle1.TextStyle.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.visualToggle1.TextStyle.TextLineAlignment = System.Drawing.StringAlignment.Center;
+            this.visualToggle1.TextStyle.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            this.visualToggle1.TrueTextToggle = "Yes";
+            this.visualToggle1.Type = VisualPlus.Toolkit.Controls.Interactivity.VisualToggle.ToggleTypes.YesNo;
             // 
             // BatchHostForm
             // 
@@ -36,16 +111,19 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.visualToggle1);
+            this.Controls.Add(this.visualGauge1);
             this.ForeColor = System.Drawing.Color.Black;
-            this.Mobile = CCWin.MobileStyle.TitleMobile;
             this.Name = "BatchHostForm";
-            this.ShowBorder = false;
             this.Text = "xQuant日志分析工具-批量宿主";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private VisualPlus.Toolkit.Controls.DataVisualization.VisualGauge visualGauge1;
+        private VisualPlus.Toolkit.Controls.Interactivity.VisualToggle visualToggle1;
     }
 }
 
