3bef9166a7eda0e75e609e49409b8929d3c678b2
Leon <xiaoyankeji@hotmail.com>
2018-12-18 16:50:51 +0800

    改造原生窗口；

--- BatchHost/BatchHostForm.cs
+++ BatchHost/BatchHostForm.cs
@@ -6,16 +6,89 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using BatchHost.Utils;
 
 namespace BatchHost
 {
     public partial class BatchHostForm : Form
     {
+        #region 属性
+        protected Pen BorderPen = new Pen(Color.LightGray, 3);
+
+        protected Brush BorderBrush { get => this.BorderPen.Brush; set => this.BorderPen.Brush = value; }
+
+        protected Color BorderColor { get => this.BorderPen.Color; set => this.BorderPen.Color = value; }
+
+        public new Icon Icon
+        {
+            get => base.Icon;
+            set
+            {
+                base.Icon = value;
+                this.TitleLabel.Image = value == null ? null : new Bitmap(value.ToBitmap(), 24, 24);
+            }
+        }
+        #endregion
+
         public BatchHostForm()
         {
             this.InitializeComponent();
 
             this.Icon = UnityResource.BatchHostIcon;
+            this.TitleLabel.MouseDown += UnityUtils.MoveFormViaMouse;
+        }
+
+        protected override void WndProc(ref Message ProcessMessage)
+        {
+            switch (ProcessMessage.Msg)
+            {
+                case UnityUtils.WM_SIZE:
+                    {
+                        base.WndProc(ref ProcessMessage);
+                        if (ProcessMessage.WParam.ToInt32() == UnityUtils.SIZE_MAXIMIZED)
+                            UnityUtils.RedrawWindow(this.Handle, IntPtr.Zero, IntPtr.Zero, UnityUtils.RDW_FRAME | UnityUtils.RDW_IUPDATENOW | UnityUtils.RDW_INVALIDATE);
+                        break;
+                    }
+                case UnityUtils.WM_NCHITTEST:
+                    {
+                        if (this.WindowState == FormWindowState.Maximized)
+                        {
+                            base.WndProc(ref ProcessMessage);
+                            break;
+                        }
+                        int nPosX = (ProcessMessage.LParam.ToInt32() & 65535);
+                        int nPosY = (ProcessMessage.LParam.ToInt32() >> 16);
+
+                        if (nPosX >= this.Right - 3 && nPosY >= this.Bottom - 3)
+                            ProcessMessage.Result = new IntPtr(UnityUtils.HT_BOTTOMRIGHT);
+                        else if (nPosX <= this.Left + 3 && nPosY <= this.Top + 3)
+                            ProcessMessage.Result = new IntPtr(UnityUtils.HT_TOPLEFT);
+                        else if (nPosX <= this.Left + 3 && nPosY >= this.Bottom - 3)
+                            ProcessMessage.Result = new IntPtr(UnityUtils.HT_BOTTOMLEFT);
+                        else if (nPosX >= this.Right - 3 && nPosY <= this.Top + 3)
+                            ProcessMessage.Result = new IntPtr(UnityUtils.HT_TOPRIGHT);
+                        else if (nPosX >= this.Right - 3)
+                            ProcessMessage.Result = new IntPtr(UnityUtils.HT_RIGHT);
+                        else if (nPosY >= this.Bottom - 3)
+                            ProcessMessage.Result = new IntPtr(UnityUtils.HT_BOTTOM);
+                        else if (nPosX <= this.Left + 3)
+                            ProcessMessage.Result = new IntPtr(UnityUtils.HT_LEFT);
+                        else if (nPosY <= this.Top + 3)
+                            ProcessMessage.Result = new IntPtr(UnityUtils.HT_TOP);
+                        else
+                            ProcessMessage.Result = new IntPtr(UnityUtils.HT_CAPTION);
+                        break;
+                    }
+                default:
+                    {
+                        base.WndProc(ref ProcessMessage);
+                        break;
+                    }
+            }
+        }
+        private void BatchHostForm_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawRectangle(this.BorderPen, 1, 1, this.Width - this.BorderPen.Width, this.Height - this.BorderPen.Width);
         }
     }
 }
