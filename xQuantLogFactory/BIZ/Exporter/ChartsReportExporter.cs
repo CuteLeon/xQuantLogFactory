3dd30e72b35f572cfa7bf2dddd30a2f61b2521fe
Leon <Leon.id@QQ.com>
2019-03-23 13:56:45 +0800

    fixed;

--- xQuantLogFactory/BIZ/Exporter/ChartsReportExporter.cs
+++ xQuantLogFactory/BIZ/Exporter/ChartsReportExporter.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
-
-using RazorEngine;
-using RazorEngine.Templating;
 
 using xQuantLogFactory.BIZ.Processer;
 using xQuantLogFactory.Model;
@@ -305,7 +303,7 @@ namespace xQuantLogFactory.BIZ.Exporter
             TaskArgument argument,
             IEnumerable<ChartContainer> chartContainers)
         {
-            string header = @"
+            string header = $@"
     <header>
         <nav class=""navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3"">
             <div class=""container"">
@@ -315,19 +313,19 @@ namespace xQuantLogFactory.BIZ.Exporter
                 </button>
                 <div class=""navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse"">
                     <ul class=""navbar-nav flex-grow-1"">
-                        @foreach(var container in Model)
-                        {
+                        {string.Join(
+                            "\n",
+                            chartContainers.Select(container => $@"
                             <li class=""nav-item"">
-                                <a class=""nav-link text-dark"" data-target=""@container.Target"">@container.Text</a>
+                                <a class=""nav-link text-dark"" data-target=""{container.Target}"">{container.Text}</a>
                             </li>
-                        }
+                        "))}
                     </ul>
                 </div>
             </div>
         </nav>
     </header>
     ";
-            header = Engine.Razor.RunCompile(header, "header", null, chartContainers);
             builder.Append(header);
         }
         #endregion
@@ -342,6 +340,104 @@ namespace xQuantLogFactory.BIZ.Exporter
         private void RenderMemory(StringBuilder builder, TaskArgument argument)
         {
             builder.AppendLine("<h1>内存</h1>");
+            string memory = $@"
+<div id=""canvas_memory"" class=""container-fluid rounded text-center text-muted"" style=""height:500px;width:800px;padding:0px""></div>
+
+<script type=""text/javascript"">
+    let memoryChart = echarts.init(document.getElementById('canvas_memory'));
+    $(window).resize(function () {{
+        memoryChart.resize();
+    }});
+    try {{
+        memoryChart.showLoading();
+
+        option = {{
+            title: {{
+                text: '折线图堆叠'
+            }},
+            tooltip: {{
+                trigger: 'axis'
+            }},
+            legend: {{
+                data: ['{string.Join("', '", argument.TerminalMonitorResults.Where(r => r.MonitorItem.Memory).Select(r => r.IPAddress).Distinct().OrderBy(ip => ip))}']
+            }},
+            grid: {{
+                left: '3%',
+                right: '4%',
+                bottom: '3%',
+                containLabel: true
+            }},
+            toolbox: {{
+                feature: {{
+                    dataZoom: {{
+                        yAxisIndex: 'none'
+                    }},
+                    restore: {{}},
+                    saveAsImage: {{}}
+                }}
+            }},
+            dataZoom: [{{
+                type: 'inside',
+            }}, {{
+                handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+                handleStyle: {{
+                    color: '#fff',
+                    shadowBlur: 3,
+                    shadowColor: 'rgba(0, 0, 0, 0.6)',
+                    shadowOffsetY: 2
+                }}
+            }}],
+            xAxis: {{
+                type: 'category',
+                boundaryGap: false,
+                data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+            }},
+            yAxis: {{
+                type: 'value'
+            }},
+            series: [
+                {{
+                    name: '邮件营销',
+                    type: 'line',
+                    stack: '总量',
+                    data: [120, 132, 101, 134, 90, 230, 210]
+                }},
+                {{
+                    name: '联盟广告',
+                    type: 'line',
+                    stack: '总量',
+                    data: [220, 182, 191, 234, 290, 330, 310]
+                }},
+                {{
+                    name: '视频广告',
+                    type: 'line',
+                    stack: '总量',
+                    data: [150, 232, 201, 154, 190, 330, 410]
+                }},
+                {{
+                    name: '直接访问',
+                    type: 'line',
+                    stack: '总量',
+                    data: [320, 332, 301, 334, 390, 330, 320]
+                }},
+                {{
+                    name: '搜索引擎',
+                    type: 'line',
+                    stack: '总量',
+                    data: [820, 932, 901, 934, 1290, 1330, 1320]
+                }}
+            ]
+        }};
+
+        memoryChart.setOption(option);
+    }} catch (err) {{
+        $('#canvas_memory').html(""加载出错，请刷新页面重试 ..."");
+    }} finally {{
+        memoryChart.hideLoading();
+    }}
+</script>
+";
+            builder.Append(memory);
         }
 
         /// <summary>
