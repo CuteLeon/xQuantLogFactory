35d1bab391f57440ee91b6e8c44957abd26c7c95
Leon <Leon.id@QQ.com>
2019-03-23 10:10:10 +0800

    modifyed code;

--- xQuantLogFactory/BIZ/Exporter/ChartsReportExporter.cs
+++ xQuantLogFactory/BIZ/Exporter/ChartsReportExporter.cs
@@ -18,6 +18,8 @@ namespace xQuantLogFactory.BIZ.Exporter
     /// </summary>
     public class ChartsReportExporter : LogProcesserBase, ILogReportExporter
     {
+        #region 数据
+
         /// <summary>
         /// 库文件目录名称
         /// </summary>
@@ -39,6 +41,7 @@ namespace xQuantLogFactory.BIZ.Exporter
         /// 图表容器
         /// </summary>
         private readonly IEnumerable<ChartContainer> chartContainers;
+        #endregion
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ChartsReportExporter"/> class.
@@ -50,12 +53,14 @@ namespace xQuantLogFactory.BIZ.Exporter
             this.chartContainers = new ChartContainer[]
             {
                 new ChartContainer("内存", "memory", this.RenderMemory),
-                new ChartContainer("客户端启动", "clientLaunch", this.RenderMemory),
-                new ChartContainer("中间件启动", "serverLaunch", this.RenderMemory),
-                new ChartContainer("事项", "monitor", this.RenderMemory),
-                new ChartContainer("请求", "performance", this.RenderMemory),
+                new ChartContainer("客户端启动", "clientLaunch", this.RenderClientLaunch),
+                new ChartContainer("中间件启动", "serverLaunch", this.RenderServerLaunch),
+                new ChartContainer("事项", "monitor", this.RenderMonitor),
+                new ChartContainer("请求", "performance", this.RenderPerformance),
             };
         }
+
+        #region 框架
 
         /// <summary>
         /// 导出日志报告
@@ -86,118 +91,6 @@ namespace xQuantLogFactory.BIZ.Exporter
             {
                 builder.Clear();
             }
-        }
-
-        /// <summary>
-        /// 渲染脚本和样式
-        /// </summary>
-        /// <param name="builder"></param>
-        private void RenderScriptStyle(StringBuilder builder)
-        {
-            string extension = string.Empty;
-            string relationPath = string.Empty;
-
-            foreach (var libFileName in this.libFileNames)
-            {
-                extension = Path.GetExtension(libFileName).ToLower();
-                relationPath = Path.Combine(LibDirName, libFileName);
-
-                switch (extension)
-                {
-                    case ".css":
-                        {
-                            builder.AppendLine($"<link rel=\"stylesheet\" href=\"{relationPath}\" />");
-                            break;
-                        }
-
-                    case ".js":
-                        {
-                            builder.AppendLine($"<script src=\"{relationPath}\"></script>");
-                            break;
-                        }
-
-                    default:
-                        break;
-                }
-            }
-        }
-
-        /// <summary>
-        /// 渲染主体
-        /// </summary>
-        /// <param name="builder"></param>
-        /// <param name="argument"></param>
-        private void RenderBody(StringBuilder builder, TaskArgument argument)
-        {
-            this.RenderHeader(builder, argument, this.chartContainers);
-
-            foreach (var container in this.chartContainers)
-            {
-                this.RenderContainer(builder, argument, container);
-            }
-        }
-
-        /// <summary>
-        /// 渲染容器
-        /// </summary>
-        /// <param name="builder"></param>
-        /// <param name="argument"></param>
-        /// <param name="container"></param>
-        private void RenderContainer(
-            StringBuilder builder,
-            TaskArgument argument,
-            ChartContainer container)
-        {
-            builder.AppendLine($@"<div class=""report-container container-fluid bg-light"" id=""container_{container.Target}"" style=""display: none"">");
-            container.Render?.Invoke(builder, argument);
-            builder.AppendLine("</div>");
-        }
-
-        /// <summary>
-        /// 渲染内存
-        /// </summary>
-        /// <param name="builder"></param>
-        /// <param name="argument"></param>
-        private void RenderMemory(StringBuilder builder, TaskArgument argument)
-        {
-            builder.AppendLine("<h1>内存</h1>");
-        }
-
-        /// <summary>
-        /// 渲染头部
-        /// </summary>
-        /// <param name="builder"></param>
-        /// <param name="argument"></param>
-        /// <param name="chartContainers"></param>
-        private void RenderHeader(
-            StringBuilder builder,
-            TaskArgument argument,
-            IEnumerable<ChartContainer> chartContainers)
-        {
-            string header = @"
-    <header>
-        <nav class=""navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3"">
-            <div class=""container"">
-                <a class=""navbar-brand"">xQuant 图表报告</a>
-                <button class=""navbar-toggler"" type=""button"" data-toggle=""collapse"" data-target="".navbar-collapse"" aria-controls=""navbarSupportedContent"" aria-expanded=""false"" aria-label=""Toggle navigation"">
-                    <span class=""navbar-toggler-icon""></span>
-                </button>
-                <div class=""navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse"">
-                    <ul class=""navbar-nav flex-grow-1"">
-                        @foreach(var container in Model)
-                        {
-                            <li class=""nav-item"">
-                                <a class=""nav-link text-dark"" data-target=""@container.Target"">@container.Text</a>
-                            </li>
-                        }
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
-    ";
-            header = Engine.Razor.RunCompile(header, "header", null, chartContainers);
-            builder.Append(header);
         }
 
         /// <summary>
@@ -266,14 +159,6 @@ namespace xQuantLogFactory.BIZ.Exporter
         }
 
         /// <summary>
-        /// 获取标题
-        /// </summary>
-        /// <param name="argument"></param>
-        /// <returns></returns>
-        private string GetTitle(TaskArgument argument)
-            => $"xQuant图标报告-{Path.GetFileName(argument.LogDirectory)}_{argument.MonitorContainerRoot.Name}";
-
-        /// <summary>
         /// 复制库文件
         /// </summary>
         private void CopyLibs()
@@ -332,5 +217,172 @@ namespace xQuantLogFactory.BIZ.Exporter
                 throw;
             }
         }
+
+        /// <summary>
+        /// 获取标题
+        /// </summary>
+        /// <param name="argument"></param>
+        /// <returns></returns>
+        private string GetTitle(TaskArgument argument)
+            => $"xQuant图标报告-{Path.GetFileName(argument.LogDirectory)}_{argument.MonitorContainerRoot.Name}";
+        #endregion
+
+        #region 渲染组件
+
+        /// <summary>
+        /// 渲染脚本和样式
+        /// </summary>
+        /// <param name="builder"></param>
+        private void RenderScriptStyle(StringBuilder builder)
+        {
+            string extension = string.Empty;
+            string relationPath = string.Empty;
+
+            foreach (var libFileName in this.libFileNames)
+            {
+                extension = Path.GetExtension(libFileName).ToLower();
+                relationPath = Path.Combine(LibDirName, libFileName);
+
+                switch (extension)
+                {
+                    case ".css":
+                        {
+                            builder.AppendLine($"<link rel=\"stylesheet\" href=\"{relationPath}\" />");
+                            break;
+                        }
+
+                    case ".js":
+                        {
+                            builder.AppendLine($"<script src=\"{relationPath}\"></script>");
+                            break;
+                        }
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 渲染主体
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="argument"></param>
+        private void RenderBody(StringBuilder builder, TaskArgument argument)
+        {
+            this.RenderHeader(builder, argument, this.chartContainers);
+
+            foreach (var container in this.chartContainers)
+            {
+                this.RenderContainer(builder, argument, container);
+            }
+        }
+
+        /// <summary>
+        /// 渲染容器
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="argument"></param>
+        /// <param name="container"></param>
+        private void RenderContainer(
+            StringBuilder builder,
+            TaskArgument argument,
+            ChartContainer container)
+        {
+            builder.AppendLine($@"<div class=""report-container container-fluid bg-light"" id=""container_{container.Target}"" style=""display: none"">");
+            container.Render?.Invoke(builder, argument);
+            builder.AppendLine("</div>");
+        }
+
+        /// <summary>
+        /// 渲染头部
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="argument"></param>
+        /// <param name="chartContainers"></param>
+        private void RenderHeader(
+            StringBuilder builder,
+            TaskArgument argument,
+            IEnumerable<ChartContainer> chartContainers)
+        {
+            string header = @"
+    <header>
+        <nav class=""navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3"">
+            <div class=""container"">
+                <a class=""navbar-brand"">xQuant 图表报告</a>
+                <button class=""navbar-toggler"" type=""button"" data-toggle=""collapse"" data-target="".navbar-collapse"" aria-controls=""navbarSupportedContent"" aria-expanded=""false"" aria-label=""Toggle navigation"">
+                    <span class=""navbar-toggler-icon""></span>
+                </button>
+                <div class=""navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse"">
+                    <ul class=""navbar-nav flex-grow-1"">
+                        @foreach(var container in Model)
+                        {
+                            <li class=""nav-item"">
+                                <a class=""nav-link text-dark"" data-target=""@container.Target"">@container.Text</a>
+                            </li>
+                        }
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    ";
+            header = Engine.Razor.RunCompile(header, "header", null, chartContainers);
+            builder.Append(header);
+        }
+        #endregion
+
+        #region 渲染图表
+
+        /// <summary>
+        /// 渲染内存
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="argument"></param>
+        private void RenderMemory(StringBuilder builder, TaskArgument argument)
+        {
+            builder.AppendLine("<h1>内存</h1>");
+        }
+
+        /// <summary>
+        /// 渲染客户端启动
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="argument"></param>
+        private void RenderClientLaunch(StringBuilder builder, TaskArgument argument)
+        {
+            builder.AppendLine("<h1>客户端启动</h1>");
+        }
+
+        /// <summary>
+        /// 渲染中间件启动
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="argument"></param>
+        private void RenderServerLaunch(StringBuilder builder, TaskArgument argument)
+        {
+            builder.AppendLine("<h1>服务端启动</h1>");
+        }
+
+        /// <summary>
+        /// 渲染监事事项
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="argument"></param>
+        private void RenderMonitor(StringBuilder builder, TaskArgument argument)
+        {
+            builder.AppendLine("<h1>监视</h1>");
+        }
+
+        /// <summary>
+        /// 渲染请求事项
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="argument"></param>
+        private void RenderPerformance(StringBuilder builder, TaskArgument argument)
+        {
+            builder.AppendLine("<h1>请求</h1>");
+        }
+        #endregion
     }
 }
