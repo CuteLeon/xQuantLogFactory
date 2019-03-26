ae6065939a45d9302de27aea473c239a70235c85
Leon <Leon.id@QQ.com>
2019-03-26 11:44:50 +0800

    BatchHost启动时自动创建生成目录；

--- BatchHost/Program.cs
+++ BatchHost/Program.cs
@@ -1,16 +1,30 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
+using BatchHost.Utils;
 
 namespace BatchHost
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
+            if (!Directory.Exists(UnityUtils.BuildDirectory))
+            {
+                try
+                {
+                    Directory.CreateDirectory(UnityUtils.BuildDirectory);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "创建生成目录失败！");
+                }
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new BatchHostForm());
