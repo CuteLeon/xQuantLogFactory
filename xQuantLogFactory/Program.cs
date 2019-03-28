872ad951416d90f8d96184fe73a690d1238e8352
Leon <Leon.id@QQ.com>
2019-03-28 15:04:00 +0800

    Performance日志版本升级：<LogTime>\s(\s<ThreadID>)?；

--- xQuantLogFactory/Program.cs
+++ xQuantLogFactory/Program.cs
@@ -1,4 +1,4 @@
-﻿#define Development
+﻿#undef Development
 
 using System;
 using System.Collections.Generic;
@@ -421,10 +421,6 @@ namespace xQuantLogFactory
             string chartsPath = string.Empty;
             bool reportSuccess = false;
             bool chartsSuccess = false;
-
-#if Development
-            chartsSuccess = true;
-#endif
 
             // 当导出失败且用户同意重试时重复导出，并在失败时再次询问用户
             do
